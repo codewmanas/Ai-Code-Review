508adba50cbf5de3aef1cc5877833a13f5b05cf1
codewmanas <manas.kolaskar@somaiya.edu>
2025-02-11 23:15:39 +0530

    Code Block Added

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect, use } from 'react'
+import "prismjs/themes/prism-tomorrow.css"
+import prism from "prismjs" 
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    prism.highlightAll()
+  })
+
+
+  return (
+    <>
+    <main>
+      <div className="left">
+        <div className="code">
+            <pre>
+              <code className="language-javascript">
+                {`
+                
+                function sum(){
+                   return 1 + 2
+                  }`
+                  }
+              </code>
+            </pre>
+        </div>
+        <div className="review">Review</div>
+      </div>
+      <div className="right">
+
+      </div>
+    </main>
+    </>
+  )
+}
+
+
+function sum(){
+  return 1 + 2
+}
+
+export default App
